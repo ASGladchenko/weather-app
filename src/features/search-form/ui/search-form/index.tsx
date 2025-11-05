b004f20cbc6ced3bb26d8e8ea6f27bb42eb190e0
ASGladchenko <asgladchenko@gmail.com>
2025-11-05 18:35:35 +0200

    create search form

---
+++ src/features/search-form/ui/search-form/index.tsx
@@ -0,0 +1,42 @@
+import { useState, useCallback } from 'react';
+
+import { cn } from '@/shared/utils';
+import { useWeatherStore } from '@/entities';
+import { Input, Button, showMessage } from '@/shared/ui';
+
+import './styles.scss';
+
+export const SearchForm = ({ className }: { className?: string }) => {
+  const [value, setValue] = useState('');
+  const { fetchWeatherByCity, status } = useWeatherStore();
+
+  const handleSubmit = useCallback(
+    async (e: React.FormEvent) => {
+      e.preventDefault();
+
+      if (!value.trim()) {
+        showMessage.warn('City name cannot be empty');
+        return;
+      }
+
+      await fetchWeatherByCity(value);
+    },
+    [fetchWeatherByCity, value]
+  );
+
+  const handleInputChange = useCallback((value: string) => {
+    setValue(value);
+  }, []);
+
+  const wrapperClassName = cn('search-form', className);
+
+  return (
+    <form onSubmit={handleSubmit} className={wrapperClassName}>
+      <Input value={value} onChange={handleInputChange} />
+
+      <Button loading={status === 'loading'} className="search-btn" type="submit">
+        Search
+      </Button>
+    </form>
+  );
+};
