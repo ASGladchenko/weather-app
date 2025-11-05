6fa91d343a61ca145a18b1ea392851a15dea38c8
ASGladchenko <asgladchenko@gmail.com>
2025-11-05 16:25:51 +0200

    connect location features

--- src/pages/layout-main/index.tsx
+++ src/pages/layout-main/index.tsx
@@ -1,6 +1,13 @@
 import { Outlet } from 'react-router-dom';
 
+import { useGeolocation } from '@/features';
+
 export const LayoutMain = () => {
+  const { isLoading } = useGeolocation();
+
+  if (isLoading) {
+    return <div>Loading...</div>;
+  }
 
   return (
     <>
